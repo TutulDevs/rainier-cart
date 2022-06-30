4a9a07e6943060ac6020f33c9ea24d3bc0634d2b
TutulDevs <tutulnahid@gmail.com>
2022-07-01 03:11:04 +0600

    added github link and readme

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,6 +1,7 @@
 import { useContext, useState } from "react";
 import AppContext from "../store/AppContext";
 import { CartSidebar } from "./CartSidebar";
+import GitHubLogo from "../images/github1.png";
 
 export const Header = () => {
   const { cartList } = useContext(AppContext);
@@ -11,14 +12,24 @@ export const Header = () => {
   return (
     <>
       <header className="h-header flex items-center justify-center bg-emerald-200">
-        <div className="container px-4 flex items-center justify-between">
-          <h2 className="text-3xl">
+        <div className="container px-4 flex gap-3 sm:gap-6 items-center justify-between">
+          <h2 className="text-xl sm:text-3xl mr-auto">
             <a href="/">Rainier Dashboard</a>
           </h2>
 
+          <a
+            href="https://github.com/TutulDevs/rainier-cart"
+            target="_blank"
+            rel="noopener noreferrer"
+            className="w-7 sm:w-9 h-7 sm:h-9 rounded-full"
+            title="GitHub Repository"
+          >
+            <img src={GitHubLogo} alt="GitHub Repository" />
+          </a>
+
           <button
             type="button"
-            className="text-3xl relative"
+            className="text-xl sm:text-3xl relative"
             aria-label="open cart"
             onClick={toggleSidebar}
           >
